bbefbdf652dd727f14e2bcb838dd8911d2b06140
Nabin Singh <nabin@skilluplabs.com.au>
2024-03-18 20:50:58 +1100

    Added populate unsettled amount functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,11 +25,20 @@
         }
 
         this.settleNow = function() {
-                // 
+               this.expenses.forEach(expense => {
+                    expense.isSettled = true;
+               }) 
         }
 
         this.calculateUnsettledAmount = function() {
-            // 
+            const usersCount = this.users.length;
+            let totalExpenses = 0;
+            this.expenses.forEach(expense => {
+                if(!expense.isSettled) {
+                    totalExpenses += expense.amount;
+                }
+            });
+            this.unsettledAmount = totalExpenses / usersCount;
         }
     }
 
@@ -40,8 +49,9 @@
         const user2 = new User("Alex", "Lee", "alex.lee@gmail.com", "0420400123", "https://api.multiavatar.com/kathrin.svg");
         const user3 = new User("Steve", "Jobs", "steve.jobs@gmail.com", "0420120163", "https://api.multiavatar.com/Binx Bond.svg");
         const user4 = new User("Elon", "Mustk", "elon.musk@gmail.com", "0420400124", "https://api.multiavatar.com/zoe.svg");
+        const user5 = new User("Jet", "Lee", "jet.lee@gmail.com", "0420400124", "https://api.multiavatar.com/kathrin.svg");
         
-        const users = [user1, user2, user3, user4];
+        const users = [user1, user2, user3, user4, user5];
         return users;
     }
 
@@ -53,22 +63,39 @@
         return allExpenses;
     }   
 
+    function removeUser(event) {
+        const index = event.target.getAttribute("data-index");
+        if((splitCostObject.users.length > 1) &&  !splitCostObject.unsettledAmount) {
+            splitCostObject.users.splice(index, 1);
+            populateUsers(splitCostObject.users);
+        }
+        
+        // if(event.target.tagName === "IMG") {
+        //     const index = event.target.getAttribute("data-index");
+        //     console.log({index});
+        // }
+    }
 
     function populateUsers(users) {
         const userContainer = document.querySelector(".users-container");
-        const userElements = users.map((user) => {
+        const userElements = users.map((user, index) => {
             return `
-            <div><img width="112" src="${user.profilePicture}" alt="${user.firstName} ${user.lastName}" /></div>
+            <div><img data-index="${index}" width="112" src="${user.profilePicture}" alt="${user.firstName} ${user.lastName}" /></div>
             `
         });
         userContainer.innerHTML = userElements.join("");
+        const imageElements = document.querySelectorAll(".users-container img");
+        imageElements.forEach(img => {
+            img.addEventListener("click", removeUser);
+        })
+        // document.querySelector(".users-container").addEventListener("click", removeUser);
     }
 
     function populateExpenses(expenses) {
         const expensesContainer = document.querySelector("#all-expenses");
         const expenseElements = expenses.map((expense) => {
             return `
-            <div ${expense.settled ? "class=settled-row" : ""}>
+            <div ${expense.isSettled ? "class=settled-row" : ""}>
                 <div>
                     <span>${expense.description}</span>
                     <time>${expense.date}</time>
@@ -82,13 +109,38 @@
         expensesContainer.innerHTML = expenseElements.join("");
     }
 
+    function populateUnsettledAmount() {
+        splitCostObject.calculateUnsettledAmount();
+        const unsettledAmount = splitCostObject.unsettledAmount;
+        const unsettledAmountElement = document.querySelector("#unsettled-amount");
+        unsettledAmountElement.textContent = unsettledAmount.toFixed(2);
+    }
+
     function addNewExpense(event) {
         event.preventDefault();
-        const description = document.querySelector("textarea").value;
-        const amount = document.querySelector("input").value;
+        const descriptionElement = document.querySelector("textarea");
+        const amountElement = document.querySelector("input");
+        const errorElement = document.querySelector("#error-msg");
+        const description = descriptionElement.value;
+        const amount = amountElement.value;
+        if(!description || !amount) {
+            errorElement.textContent = "Description and amount are required";
+            return
+        };
         const newExpense = new ExpensesItem(description, parseFloat(amount));
         splitCostObject.addNewExpenses(newExpense);
         populateExpenses(splitCostObject.expenses);
+        populateUnsettledAmount();
+
+        descriptionElement.value = "";
+        amountElement.value = "";
+        errorElement.textContent = "";
+    }
+
+    function settleExpenses() {
+        splitCostObject.settleNow();
+        populateExpenses(splitCostObject.expenses);
+        populateUnsettledAmount();
     }
 
     function addNewEventListener() {
@@ -96,8 +148,13 @@
         newButtonElement.addEventListener("click", addNewExpense);
     }
 
+    function addSettleNowEventListner() {
+        const settleNowButton = document.querySelector("#settle-now-btn");
+        settleNowButton.addEventListener("click", settleExpenses);
+    }
 
     addNewEventListener();
+    addSettleNowEventListner();
 
     const splitCostObject = new SplitCost();
     populateUsers(splitCostObject.users);
